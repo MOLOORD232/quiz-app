e10ef38c188683c4eaaf74ee99ff76e0b4fa8110
MOLOORD232 <Mdmdijmzjwjf@gmail.com>
2025-01-06 21:46:01 +0200

    Update button.jsx

--- components/ui/button.jsx
+++ components/ui/button.jsx
@@ -1,11 +1,10 @@
-import '../styles/globals.css'
-
-function MyApp({ Component, pageProps }) {
+export function Button({ children, className = '', ...props }) {
   return (
-    <div dir="rtl">
-      <Component {...pageProps} />
-    </div>
-  )
+    <button
+      className={`px-4 py-2 rounded ${className}`}
+      {...props}
+    >
+      {children}
+    </button>
+  );
 }
-
-export default MyApp
