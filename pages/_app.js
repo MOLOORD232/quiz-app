fd04122940f8b074270082b7649dddb0bc187460
MOLOORD232 <Mdmdijmzjwjf@gmail.com>
2025-01-06 21:59:39 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,23 @@
-// pages/_app.js
-// Change from:
-// import '@/styles/globals.css'
-// To:
 import '../styles/globals.css'
+import { useEffect } from 'react'
+import { getAnalytics, isSupported } from 'firebase/analytics'
+import { app } from '../firebase/config'
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    // تهيئة Firebase Analytics فقط في جانب العميل (browser)
+    const initAnalytics = async () => {
+      try {
+        if (await isSupported()) {
+          getAnalytics(app);
+        }
+      } catch (error) {
+        console.log('Analytics error:', error);
+      }
+    };
+
+    initAnalytics();
+  }, []);
+
   return <Component {...pageProps} />
 }
