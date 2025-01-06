ff0350f310a7e646436dd4dd97a8f7feeee94bc1
MOLOORD232 <Mdmdijmzjwjf@gmail.com>
2025-01-06 16:48:11 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,8 @@
-import '../styles/globals.css';
+// pages/_app.js
+// Change from:
+// import '@/styles/globals.css'
+// To:
+import '../styles/globals.css'
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
